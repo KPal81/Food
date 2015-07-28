d9924a1788055b8dba5ce66c6abf3ca7af790788
KPal81 <krisbamrah@gmail.com>
2015-07-28 13:05:21 +0100

    Ingredients

--- test1.php
+++ test1.php
@@ -1,11 +1,20 @@
-<form action="test1.php" method="POST">
+<html>
+<style>
+#sty {
+	text-align: center;
+}
+</style>
+<div id="sty">
+<form align="center" action="test1.php" method="POST" >
 <input type="text" name="input" />
 </form>
+</div>
+</html>
 <?php
 	if(isset($_POST['input'])){
 		$input = $_POST['input']; 
-		$myfile = fopen("ing.txt", "a+") or die("Unable to open file!");
-		fwrite($myfile, $input);
+		$myfile = fopen("ing.txt", "a") or die("Unable to open file!");
+		fwrite($myfile, $input.PHP_EOL);
 		fclose($myfile);
 	}
 ?>
